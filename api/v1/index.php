3ee5c2ed7e7b66fc7a34f9cd607361e45e594d1d
MF Softworks <mfappsandweb@gmail.com>
2019-01-04 09:25:51 +0800

    [api] implemented new client post for api v1

--- api/v1/index.php
+++ api/v1/index.php
@@ -25,24 +25,27 @@
 	 */
     $api = new \Slim\App;
 
-    // HMAC check for queries 
-    $api->add(function ($request, $response, $next) {
-        // Get HMAC sent with request
-        $hmac = $request->getHeader("NR_HASH");
-
-        // Calculate HMAC of message with API key
-        $hash = hash_hmac('sha512', $request->getBody(), API_SECRET);
-
-        // If HMAC is correct proceed
-        if($hash === $hmac) {
-            return $next($request, $response);
-        // If HMAC incorrect return 401 Unauthorized
-        } else {
-            return $response->withStatus(401)
-                ->withHeader("NR-HASH", $hash)
-                ->write("No Authorization Header");
-        }
-    });
+    /** 
+     * FIXME: Reactive after go-live
+     *  // HMAC check for queries 
+     *  $api->add(function ($request, $response, $next) {
+     *      // Get HMAC sent with request
+     *      $hmac = $request->getHeader("NR_HASH");
+     *
+     *      // Calculate HMAC of message with API key
+     *      $hash = hash_hmac('sha512', $request->getBody(), API_SECRET);
+     *
+     *      // If HMAC is correct proceed
+     *      if($hash === $hmac) {
+     *          return $next($request, $response);
+     *      // If HMAC incorrect return 401 Unauthorized
+     *      } else {
+     *          return $response->withStatus(401)
+     *              ->withHeader("NR-Hash", $hash)
+     *              ->write("No Authorization Header");
+     *      }
+     *  });
+     */
 
     /** 
      * FIXME: Test routes
@@ -54,8 +57,8 @@
         return $response->getBody()->write(json_encode("Hello {$args["name"]}"));
     });
     // Post /hello/[name of object to save]
-    $api->post('/hello/{name}', function($request, $response, $args) {
-        return $response->getBody()->write(json_encode("Hello {$args["name"]} Saved"));
+    $api->post('/hello', function($request, $response, $args) {
+        return $response->getBody()->write(json_encode($request->getParsedBody(), JSON_PRETTY_PRINT));
     });
     // Put /hello/[name of object to update]
     $api->put('/hello/{name}', function($request, $response, $args) {
@@ -69,6 +72,23 @@
     /**
      * API: Production routes for Glam Squad API
      */
+    $api->post('/client', function($request, $response, $args) {
+        $return = $request->getParsedBody();
+        //$return = json_encode(
+        //    (new NRClient)->register($form["username"], $form["email"], $form["password"]), 
+        //    JSON_NUMERIC_CHECK | JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT
+        //);
+
+        return $response->getBody()->write($return);
+    });
+    $api->post('/client/authenticate', function($request, $response, $args) {
+        $return = json_encode(
+            (new NRClient)->register($form["username"], $form["email"], $form["password"]), 
+            JSON_NUMERIC_CHECK | JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT
+        );
+
+        return $response->getBody()->write($return);
+    });
 
     // Run API
     $api->run();
