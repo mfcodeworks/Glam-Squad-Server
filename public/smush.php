f8e95a73f95373176e00f0dec3ccd19c6f169ad2
MF Softworks <mfappsandweb@gmail.com>
2019-01-08 16:31:57 +0800

    [resmushit] fixed smush path

--- public/smush.php
+++ public/smush.php
@@ -6,7 +6,7 @@
     ignore_user_abort(true);
 
 	// Paths
-    define('PROJECT_ROOT', dirname(__FILE__));
+    define('PROJECT_ROOT', dirname(dirname(__FILE__)));
     define('PROJECT_CONFIG', PROJECT_ROOT . '/config/');
 	define('PROJECT_INC', PROJECT_ROOT . '/src/');
 	define('PROJECT_LIB', PROJECT_ROOT . '/vendor/');
