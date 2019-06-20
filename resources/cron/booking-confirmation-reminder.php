fa2649965ab9d8b4937f62492956142ade0f4038
MF Softworks <mfappsandweb@gmail.com>
2019-06-20 09:52:01 +0800

    [cron] reformatted booking confirmation reminder script

--- resources/cron/booking-confirmation-reminder.php
+++ resources/cron/booking-confirmation-reminder.php
@@ -17,84 +17,86 @@
     require_once PROJECT_INC . "NRPackage.php";
     require_once PROJECT_LIB . "autoload.php";
 
+    // Log cron task
     error_log("[".date('Y-m-d H:i:s')."] Sending recent event confirmation reminders.");
 
-    // Select events that have passed an hour with less than 3 hours which remain unpaid
-    $sql =
-    "SELECT j.id
-        FROM nr_jobs as j
-        LEFT JOIN nr_client_receipts as r ON j.id = r.event_id
-        WHERE r.event_id IS NULL
-        AND TIMESTAMPDIFF(MINUTE, NOW(), j.event_datetime) > -60
-        AND TIMESTAMPDIFF(MINUTE, NOW(), j.event_datetime) <= -180;";
+    // Init. FCM object
+    $fcm = new NRFCM();
 
     // Get list of events
-    $query = runSQLQuery($sql);
+    $events = getRecentlyCompletedEvents();
 
-    // If no events, exit
-    if(!isset($query["data"])) die(0);
+    // Loop event ID list, if reminder hasn't been sent, send reminder
+    foreach($events as $eventObject) {
+        if(!reminderSent($eventObject["id"])) constructReminder($eventObject);
+    }
 
-    // Loop event ID list
-    foreach($query["data"] as $eventObject) {
+    function constructReminder($eventObject) {
+        // Get event info
+        $event = (new NREvent())->getSingle($eventObject["id"]);
 
-        // If reminder hasn't been sent
-        if(reminderSent($eventObject["id"]) === false) {
+        // Format address for notifications
+        $addressArray = explode(",", $event->address);
+        $notifAddress = $addressArray[0];
+        (isset($addressArray[1])) ? $notifAddress .= "," . $addressArray[1] : null;
+        (isset($addressArray[2])) ? $notifAddress .= "," . $addressArray[2] : null;
 
-            // Get event info
-            $event = new NREvent();
-            $event->getSingle($eventObject["id"]);
+        // Create notification payload
+        $notification = [
+            "condition" => "'event-{$event->id}-client' in topics || 'event-{$event->id}-artist' in topics",
+            "priority" => "high",
+            "data" => [
+                "title" => "Event Confirmation",
+                "message" => "It looks like the event at {$notifAddress} is finished. Don't forget to confirm the event for payment.",
+                "content-available"  => "1",
+                "image" => "logo"
+            ]
+        ];
 
-            // Init. FCM object
-            $fcm = new NRFCM();
+        // DEBUG: Log notification
+        error_log(json_encode($notification));
 
-            // Format address for notifications
-            $addressArray = explode(",", $event->address);
-            $notifAddress = $addressArray[0];
-            (isset($addressArray[1])) ? $notifAddress .= "," . $addressArray[1] : null;
-            (isset($addressArray[2])) ? $notifAddress .= "," . $addressArray[2] : null;
-
-            // Create notification payload
-            $notification = [
-                "condition" => "'event-{$event->id}-client' in topics || 'event-{$event->id}-artist' in topics",
-                "priority" => "high",
-                "data" => [
-                    "title" => "Event Confirmation",
-                    "message" => "It looks like the event at {$notifAddress} is finished. Don't forget to confirm the event for payment.",
-                    "content-available"  => "1",
-                    "image" => "logo"
-                ]
-            ];
-
-            // Try to send reminder notification
-            try {
-                $fcm->send($notification, FCM_NOTIFICATION_ENDPOINT);
-                setReminderSent($event->id);
-            }
-            catch(Exception $e) {
-                error_log("Error sending reminder for event {$event->id}");
-            }
+        // Try to send reminder notification
+        try {
+            $fcm->send($notification, FCM_NOTIFICATION_ENDPOINT);
+            setReminderSent($event->id);
+        } catch(Exception $e) {
+            error_log("Error sending reminder for event {$event->id}");
         }
     }
 
-    // Check reminder sent
+    function getRecentlyCompletedEvents() {
+        // Select events that have passed an hour with less than 3 hours which remain unpaid
+        $query = runSQLQuery(
+            "SELECT j.id
+            FROM nr_jobs as j
+            LEFT JOIN nr_client_receipts as r ON j.id = r.event_id
+            WHERE r.event_id IS NULL
+            AND TIMESTAMPDIFF(HOUR, j.event_datetime, NOW()) >= 1
+            AND TIMESTAMPDIFF(HOUR, j.event_datetime, NOW()) <= 3;"
+        )["data"];
+
+        // Return found events or empty array
+        return $query ? $query : [];
+    }
+
     function reminderSent($id) {
         // Check for reminder
-        $sql =
-        "SELECT id
+        $data = runSQLQuery(
+            "SELECT id
             FROM nr_job_confirmation_reminders
-            WHERE event_id = $id;";
+            WHERE event_id = $id;"
+        );
 
-        $data = runSQLQuery($sql);
-
-        if(isset($data["data"][0])) return true;
-        return false;
+        // Return true if reminder has been sent
+        return isset($data["data"][0]);
     }
 
     function setReminderSent($id) {
-        $sql =
-        "INSERT INTO nr_job_confirmation_reminders(event_id)
-            VALUES($id);";
-
-        return runSQLQuery($sql)["response"];
+        // Set reminder as sent
+        return runSQLQuery(
+            "INSERT INTO nr_job_confirmation_reminders(event_id)
+            VALUES($id);"
+        )["response"];
     }
 ?>
